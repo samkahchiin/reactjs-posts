a8e710ee275ca5b8d3857bc804507afecd5e2cc7
samkahchiin <samkahchiin@gmail.com>
2020-05-28 20:59:08 +0800

    [Finishes] Used NavLink instead of Link so we can implement CSS style

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Posts from './Posts/Posts';
 import NewPost from './NewPost/NewPost';
-import { Route, Link } from 'react-router-dom';
+import { Route, NavLink } from 'react-router-dom';
 import './Blog.css';
 
 class Blog extends Component {
@@ -10,12 +10,12 @@ class Blog extends Component {
       <div className="Blog">
         <header>
           <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to={{
+            <li><NavLink to="/" exact>Home</NavLink></li>
+            <li><NavLink to={{
               pathname: '/new-post',
               hash: '#submit',
               search: '?quick-submit=true'
-            }}>New Post</Link></li>
+            }}>New Post</NavLink></li>
           </ul>
         </header>
         {/* <Route path="/" exact render={() => <h1>Home</h1>} />

b19c8932ae8a23cf796c4dd9643a2e8f6861cace
samkahchiin <samkahchiin@gmail.com>
2020-05-28 21:14:10 +0800

    [Finishes] Added dynamic link to access Post object from Posts

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Posts from './Posts/Posts';
 import NewPost from './NewPost/NewPost';
+import FullPost from './FullPost/FullPost';
 import { Route, NavLink } from 'react-router-dom';
 import './Blog.css';
 
@@ -22,6 +23,7 @@ class Blog extends Component {
         <Route path="/" render={() => <h1>Home 2</h1>} /> */}
         <Route path="/" exact component={Posts} />
         <Route path="/new-post" component={NewPost} />
+        <Route path="/:id" component={FullPost} />
       </div>
     );
   }

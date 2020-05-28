207c5b8b74951c7ff71be57386d951521290547c
samkahchiin <samkahchiin@gmail.com>
2020-05-28 17:50:10 +0800

    [Chore] Setup the project for routing

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,66 +1,25 @@
 import React, { Component } from 'react';
-import axios from '../../axios';
-import Post from '../../components/Post/Post';
+import Posts from './Posts/Posts';
+import { Route } from 'react-router-dom';
 import './Blog.css';
 
 class Blog extends Component {
-    state = {
-      posts: [],
-      selectedPostId: null,
-      error: false
-    }
+  render () {
+    return (
+      <div className="Blog">
+        <header>
+          <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="/new-post">New Post</a></li>
+          </ul>
+        </header>
+        {/* <Route path="/" exact render={() => <h1>Home</h1>} />
+        <Route path="/" render={() => <h1>Home 2</h1>} /> */}
 
-    componentDidMount() {
-      axios.get("/posts")
-        .then(response => {
-          const posts = response.data.slice(0,4);
-          const updatedPost = posts.map(post => {
-            return {
-              ...post,
-              author: "Max"
-            }
-          })
-          this.setState({
-            posts: updatedPost
-          })
-        })
-        .catch(error =>
-          this.setState({ error: true })
-        )
-    }
-
-    postSelectedHandler = (id) => {
-      this.setState({
-        selectedPostId: id
-      })
-    }
-
-    render () {
-      let posts = <p style={{textAlign: 'center'}}>Something went wrong</p>
-      if (!this.state.error) {
-        posts = this.state.posts.map(post => {
-          return <Post
-            key={post.id}
-            title={post.title}
-            author={post.author}
-            clicked={() => this.postSelectedHandler(post.id)}/>;
-        });
-      }
-
-      return (
-        <div className="Blog">
-          <header>
-            <ul>
-              <li><a href="/">Home</a></li>
-              <li><a href="/new-post">New Post</a></li>
-            </ul>
-          </header>
-          <section className="Posts">
-            {posts}
-          </section>
-        </div>
-      );
-    }
+        <Posts />
+      </div>
+    );
+  }
 }
 
 export default Blog;
